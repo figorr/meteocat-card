c77f6226bffe84f12768d4ffbdc869a3c60dcfb0
figorr <jdcuartero@yahoo.es>
2025-09-19 17:02:20 +0200

    update rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,50 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser"; // Usamos import por defecto
 
-export default {
-  input: "src/meteocat-card.js",
-  output: {
-    file: "dist/meteocat-card.js",
-    format: "es",
-    sourcemap: false,
+export default [
+  // Meteocat Card
+  {
+    input: "src/meteocat-card.js",
+    output: {
+      file: "dist/meteocat-card.js",
+      format: "es",
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(), // minimiza el JS
+    ],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-};
+
+  // Editor de la card
+  {
+    input: "src/meteocat-card-editor.js",
+    output: {
+      file: "dist/meteocat-card-editor.js",
+      format: "es",
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
+  },
+
+  // Loader para instalación manual
+  {
+    input: "src/loader.js",
+    output: {
+      file: "dist/loader.js",
+      format: "es",
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
+  },
+];
